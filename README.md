92504df4cf7390f929f8c958b8ed996fccfde494
Robert Schiemann <rschiemann@Robert-Schiemann-MBP.local>
2017-04-17 14:06:45 -0700

    Updated kallisto image. Removed index and added shell script to run kallisto.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # kallisto
-Contains a Dockerfile that builds an image with [Kallisto](https://pachterlab.github.io/kallisto/)
-and an index for homo_sapiens_GRCh38. Index is copied in as Kallisto currently fails building the index during the Docker build process.
+Contains a Dockerfile that builds an image with [Kallisto](https://pachterlab.github.io/kallisto/) for use with Pachyderm.
---
+++ code/run_kallisto.sh
@@ -0,0 +1,45 @@
+set -euo pipefail
+
+# Setting up input and output directories.
+INPUT_DIR='/'
+OUTPUT_DIR='/'
+INDEX_DIR='/'
+
+# Getting input and output directories from arguements
+if [ $# -ne 4 ]; then
+	INPUT_DIR=$(readlink -f $1)
+	OUTPUT_DIR=$(readlink -f $2)
+	INDEX_DIR=$(readlink -f $3)
+	echo "Using $INPUT_DIR as input directory, $OUTPUT_DIR as output directory, and $INDEX_DIR as index directory"
+else
+	echo "Usage: atac_pipeline.sh /directory/to/analyze /directory/to/output /directory/with/indices"
+	exit 1
+fi
+
+cd $INPUT_DIR
+
+# Iterating through all of the experiments in the input directory.
+for dir in */
+do
+	DIR=${dir%*/}
+
+	# Set up output directory
+	CUR_OUTPUT_DIR="${OUTPUT_DIR}/${DIR}"
+	mkdir $CUR_OUTPUT_DIR
+
+	cd $DIR
+	CUR_INPUT_DIR="${INPUT_DIR}/${DIR}"
+	cd $CUR_INPUT_DIR
+	echo "Analyzing files in ${CUR_INPUT_DIR}"
+
+	for i in `ls *1.fastq.gz`;
+	do
+	 	sampleID="${i%1.*.*}"
+	 	SAMPLE_OUTPUT_DIR="${CUR_OUTPUT_DIR}/${sampleID}" 
+	 	mkdir $SAMPLE_OUTPUT_DIR
+	  	echo "Analyzing sample ${sampleID}"
+	 	kallisto quant -i ${INDEX_DIR}/Homo_sapiens.GRCh38.87.cdna.all.idx -o $SAMPLE_OUTPUT_DIR -b 100 -t 8 ${sampleID}1.fastq.gz ${sampleID}2.fastq.gz
+	done
+
+	echo $?
+done
